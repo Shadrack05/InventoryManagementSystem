0c14aab3cccf1d533b9a9fe06aeb997ed1236bab
shadrack <shadrackomuya5@gmail.com>
2025-06-03 10:12:00 +0300

    initial

---
+++ resources/js/app.js
@@ -0,0 +1,13 @@
+import './bootstrap';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
+import { createApp } from 'vue';
+import App from './components/App.vue';
+
+const app = createApp(App);
+app.mount('#app');
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+    <html :class="{ 'theme-dark': dark }" lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>Imanisoft Dashboard</title>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css"
+          />
+          <link rel="icon" type="image/png" href="{{ asset('images/app-icon.png') }}">
+          <meta name="csrf-token" content="{{ csrf_token() }}">
+        </head>
+<body>
+    <div id="app">
+        <dashboard csrf-token="{{ csrf_token() }}"></dashboard>
+    </div>
+
+    <!-- Include the compiled JavaScript -->
+    @vite('resources/js/app.js')
+</body>
+</html>
