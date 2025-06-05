34b1ef12f670ba8555f1e626f0678443323b10a0
shadrack <shadrackomuya5@gmail.com>
2025-06-05 16:32:49 +0300

    mobile view

--- resources/js/components/dashboard.vue
+++ resources/js/components/dashboard.vue
@@ -666,6 +666,94 @@
               </ul>
             </template>
           </li>
+                  <li v-if="Roles.includes('admin')" class="relative px-6 py-3">
+            <span
+            v-if="currentView === 'AdminComponent'"
+              class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+              aria-hidden="true"
+            ></span>
+            <a
+             :class="{ 'dark:text-gray-100': currentView ==='MessageComponent'}"
+              class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+              href="#"
+              @click="currentView = 'AdminComponent'"
+            >
+              <svg
+                class="w-5 h-5"
+                aria-hidden="true"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
+                ></path>
+              </svg>
+              <span class="ml-4">Users</span>
+            </a>
+          </li>
+
+        <li v-if="Roles.includes('admin')" class="relative px-6 py-3">
+            <span
+            v-if="currentView === 'RolesComponent'"
+              class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+              aria-hidden="true"
+            ></span>
+            <a
+             :class="{ 'dark:text-gray-100': currentView ==='MessageComponent'}"
+              class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+              href="#"
+              @click="currentView = 'RolesComponent'"
+            >
+              <svg
+                class="w-5 h-5"
+                aria-hidden="true"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
+                ></path>
+              </svg>
+              <span class="ml-4">Roles</span>
+            </a>
+          </li>
+        <li class="relative px-6 py-3">
+            <span
+            v-if="currentView === 'ProductsComponent'"
+              class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+              aria-hidden="true"
+            ></span>
+            <a
+             :class="{ 'dark:text-gray-100': currentView ==='MessageComponent'}"
+              class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+              href="#"
+              @click="currentView = 'ProductsComponent'"
+            >
+              <svg
+                class="w-5 h-5"
+                aria-hidden="true"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
+                ></path>
+              </svg>
+              <span class="ml-4">Products</span>
+            </a>
+          </li>
         </ul>
         <div class="px-6 my-6">
         </div>
